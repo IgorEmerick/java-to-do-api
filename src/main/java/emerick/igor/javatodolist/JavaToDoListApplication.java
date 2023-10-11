2f2f07820b4b3bee379964b7bde104629376371d
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-10-11 06:59:56 -0300

    feature: start project

---
+++ src/main/java/emerick/igor/javatodolist/JavaToDoListApplication.java
@@ -0,0 +1,13 @@
+package emerick.igor.javatodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emerick/igor/javatodolist/JavaToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package emerick.igor.javatodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
