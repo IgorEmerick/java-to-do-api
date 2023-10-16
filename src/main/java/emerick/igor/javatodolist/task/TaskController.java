defcb334ef1276803ebc60b28c371ab736671e1a
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-10-16 17:44:51 -0300

    feature: add route to list user tasks

--- src/main/java/emerick/igor/javatodolist/task/TaskController.java
+++ src/main/java/emerick/igor/javatodolist/task/TaskController.java
@@ -1,10 +1,12 @@
 package emerick.igor.javatodolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,5 +49,14 @@ public class TaskController {
     TaskModel task = this.taskRepository.save(taskModel);
 
     return ResponseEntity.status(201).body(task);
+  }
+
+  @GetMapping("/")
+  public ResponseEntity getTasks(HttpServletRequest request) {
+    UUID userId = (UUID) request.getAttribute("userId");
+    
+    List<TaskModel> tasks = this.taskRepository.findByUserId(userId);
+
+    return ResponseEntity.ok(tasks);
   }
 }
