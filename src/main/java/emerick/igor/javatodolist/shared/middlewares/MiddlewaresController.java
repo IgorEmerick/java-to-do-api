710aa37a8c20aa13ab51131491c23161837214b2
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-10-19 19:11:46 -0300

    update: change the way to apply authentication to routes

---
+++ src/main/java/emerick/igor/javatodolist/shared/middlewares/MiddlewaresController.java
@@ -0,0 +1,26 @@
+package emerick.igor.javatodolist.shared.middlewares;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import emerick.igor.javatodolist.modules.user.database.repositories.models.IUserRepository;
+import emerick.igor.javatodolist.shared.filters.AuthenticationFilter;
+
+@Configuration
+public class MiddlewaresController {
+  @Autowired
+  IUserRepository userRepository;
+  
+  @Bean
+  FilterRegistrationBean<AuthenticationFilter> registerAuthenticationMiddleware() {
+    FilterRegistrationBean<AuthenticationFilter> registrationBean = new FilterRegistrationBean<AuthenticationFilter>();
+
+    registrationBean.setFilter(new AuthenticationFilter(this.userRepository));
+    registrationBean.addUrlPatterns("/task/*");
+    registrationBean.setOrder(0);
+
+    return registrationBean;
+  }
+}
