0f7da801dc1758e73ca5babb97dbf7d7675fae13
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-10-24 18:52:44 -0300

    update: delete all

--- src/main/java/emerick/igor/javatodolist/shared/providers/models/IHashProvider.java
+++
@@ -1,7 +0,0 @@
-package emerick.igor.javatodolist.shared.providers.models;
-
-public interface IHashProvider {
-  public String getHash(String phrase);
-
-  public Boolean compare(String phrase, String hash);
-}
