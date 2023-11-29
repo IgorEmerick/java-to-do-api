0fd2bcb0489f4b002d19478ddd2e818f0a56d82d
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-11-29 18:30:54 -0300

    database: add StageEntity

---
+++ src/main/java/emerick/igor/javatodolist/modules/to_do_list/database/entities/StageEntity.java
@@ -0,0 +1,39 @@
+package emerick.igor.javatodolist.modules.to_do_list.database.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "stages")
+public class StageEntity {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  private String name;
+
+  @Column(name = "project_id")
+  private String projectId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "project_id", insertable = false, updatable = false)
+  private ProjectEntity project;
+}
