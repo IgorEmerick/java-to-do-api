41321963585569d94d3c3ff92235d4614ce739d5
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-11-29 18:34:54 -0300

    database: update StageEntity properties type and constructors

---
+++ src/main/java/emerick/igor/javatodolist/modules/to_do_list/services/StageService.java
@@ -0,0 +1,11 @@
+package emerick.igor.javatodolist.modules.to_do_list.services;
+
+import org.springframework.stereotype.Service;
+
+import emerick.igor.javatodolist.modules.to_do_list.database.entities.StageEntity;
+
+@Service
+public class StageService {
+  public StageEntity create() {
+  }
+}
